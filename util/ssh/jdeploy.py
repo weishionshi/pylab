4a25a16a4ae0baf753bcdf7f8670e94036262f2d
weishionshi <weishionshi@163.com>
2020-04-08 17:44:03 +0800

    create

---
+++ util/ssh/jdeploy.py
@@ -0,0 +1,12 @@
+import sys
+from util.ssh.jenkins_deploy import JenkinsDeployer
+
+if __name__ == '__main__':
+    if sys.argv[1] == '' :
+        print('请指定环境信息，例如jdeploy fintcs-tcs-service')
+        exit()
+    else:
+        # init deployer obj
+        obj = JenkinsDeployer(sys.argv[1])
+        # start deploy
+        obj.deploy()
