7dcbbbe3e17b3ce29c405ac269fd3d6ff76e9e5a
weishionshi <weishionshi@163.com>
2020-10-16 20:33:19 +0800

    create:lcs auto script

---
+++ seepp/ssh/lcs.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author  : shiwx27477
+# @time    : 2020/10/16 10:40
+# @file    : lcs.py
+
+
+
+"""
+sync servers' datetime
+"""
+def sync_server_time:
+    pass
+
+"""
+update qrtz_triggers.next_fire_time
+"""
+def update_qrtz_triggers:
+    # get lcs/tcs db connection config
+
+    # get conns
+
+    # exec update sql
+    pass
+
+"""
+backup database
+"""
+def backup_db:
+    pass
+
+
+def get_sysdate:
+    pass
+
+"""
+scan pre conditions:
+    - sysdate
+    - next_fire_time
+"""
+
+if __name__ == '__main__':
+    update_qrtz_triggers()
